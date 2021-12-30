9155cc6471bea19af8b3fb65846437b2c9edffd7
nikosNalmpantis <nikosnalmpantis@outlook.com>
2021-12-31 00:33:57 +0200

    feat(update): start image

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "https://github.com/nikosNalmpantis"
 end
 
 dashboard.section.footer.val = footer()
