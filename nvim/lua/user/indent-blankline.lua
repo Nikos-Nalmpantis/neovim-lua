8f3de12ffd8dfa004fd263d6f0d1e70d0d7d9c98
nikosNalmpantis <nikosnalmpantis@outlook.com>
2022-11-11 14:47:16 +0100

    fix(remove): show current context start

--- nvim/lua/user/indent-blankline.lua
+++ nvim/lua/user/indent-blankline.lua
@@ -7,5 +7,5 @@ indent_blankline.setup {
   char = '·', -- Basic Char
   context_char = '⍿', -- Char when inside indentantion
   show_current_context = true, -- Changes to context_char when indsiede indentantion
-  show_current_context_start = true, -- Underscores show current context start
+  show_current_context_start = false, -- Underscores show current context start
 }
