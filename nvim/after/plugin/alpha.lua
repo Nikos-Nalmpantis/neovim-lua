9e9976cc60aed0a92492addf35a7dbfa30a3fe13
nikosNalmpantis <nikosnalmpantis@outlook.com>
2023-01-06 12:27:15 +0100

    feat(update): alpha emojis

--- nvim/after/plugin/alpha.lua
+++ nvim/after/plugin/alpha.lua
@@ -19,13 +19,14 @@ dashboard.section.header.val = {
   [[             ░        ░                            ░      ]],
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/lua/plugins.lua<CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+	dashboard.button("f", "🔍 Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", "📄 New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "📁 Find project", ":Telescope projects <CR>"),
+	dashboard.button("r", "🕒 Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "🇹 Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", "⚙️  Configuration", ":e ~/.config/nvim/lua/plugins.lua<CR>"),
+  dashboard.button("s", "🦥 Lazy Sync", ":Lazy sync<CR>"),
+	dashboard.button("q", "❌ Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
